679b641ff8f01ab7caa2745a8b7db3a87d5d1148
Abdullah Alamoodi <abady1000@gmail.com>
2022-05-10 20:18:41 +0300

    generated files from MerkleTree.circom

--- Part1/test/merkle-test.js
+++ Part1/test/merkle-test.js
@@ -74,3 +74,10 @@ describe("MerkleTree", function () {
         // [bonus] verify the second leaf with the inclusion proof
     });
 });
+/**
+ * 
+ *           14
+ *     12          13
+ *  8     9     10    11
+ * 0  1  2  3  4  5  6  7
+ */
